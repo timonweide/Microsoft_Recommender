fdacee4520fddc298e2d43754d9c96869947eee7
Timon Heinrich Weidemann <tim@timonweidemann.de>
2025-06-18 15:04:45 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -18,6 +18,8 @@ import cohere
 df = pd.read_csv("clean_data_model.csv")
 product_descriptions = pd.read_csv("clean_product_descriptions.csv")
 
+st.markdown(product_descriptions.head())
+
 # --- Preparation ---
 df['related_list'] = df['related_products'].apply(eval)
 issue_cols = [
